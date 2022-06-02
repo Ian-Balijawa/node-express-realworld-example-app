f9c80a9028198a53120ef458de3f48be79d069f7
ian <ianbalijawa16@gmail.com>
2022-06-02 18:14:28 +0300

    wired up basic app

--- app.js
+++ app.js
@@ -1,85 +1,98 @@
-var http = require('http'),
-    path = require('path'),
-    methods = require('methods'),
-    express = require('express'),
-    bodyParser = require('body-parser'),
-    session = require('express-session'),
-    cors = require('cors'),
-    passport = require('passport'),
-    errorhandler = require('errorhandler'),
-    mongoose = require('mongoose');
+// var http = require('http'),
+//     path = require('path'),
+//     methods = require('methods'),
+//     express = require('express'),
+//     bodyParser = require('body-parser'),
+//     session = require('express-session'),
+//     cors = require('cors'),
+//     passport = require('passport'),
+//     errorhandler = require('errorhandler'),
+//     mongoose = require('mongoose');
 
-var isProduction = process.env.NODE_ENV === 'production';
+// var isProduction = process.env.NODE_ENV === 'production';
 
-// Create global app object
-var app = express();
+// // Create global app object
+// var app = express();
 
-app.use(cors());
+// app.use(cors());
 
-// Normal express config defaults
-app.use(require('morgan')('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// // Normal express config defaults
+// app.use(require('morgan')('dev'));
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
-app.use(require('method-override')());
-app.use(express.static(__dirname + '/public'));
+// app.use(require('method-override')());
+// app.use(express.static(__dirname + '/public'));
 
-app.use(session({ secret: 'conduit', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false  }));
+// app.use(session({ secret: 'conduit', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false  }));
 
-if (!isProduction) {
-  app.use(errorhandler());
-}
+// if (!isProduction) {
+//   app.use(errorhandler());
+// }
 
-const MONGO_URI = "mongodb+srv://ianbalijawa:3X2qt8NT6iPWLsEm@cluster0.fl3nd.mongodb.net/?retryWrites=true&w=majority";
-if(isProduction){
-  mongoose.connect(MONGO_URI);
-} else {
-  mongoose.connect('mongodb://localhost/conduit');
-  mongoose.set('debug', true);
-}
+// const MONGO_URI = "mongodb+srv://ianbalijawa:3X2qt8NT6iPWLsEm@cluster0.fl3nd.mongodb.net/?retryWrites=true&w=majority";
+// if(isProduction){
+//   mongoose.connect(MONGO_URI);
+// } else {
+//   mongoose.connect('mongodb://localhost/conduit');
+//   mongoose.set('debug', true);
+// }
 
-require('./models/User');
-require('./models/Article');
-require('./models/Comment');
-require('./config/passport');
+// require('./models/User');
+// require('./models/Article');
+// require('./models/Comment');
+// require('./config/passport');
 
-app.use(require('./routes'));
+// app.use(require('./routes'));
 
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+// /// catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+
+// /// error handlers
+
+// // development error handler
+// // will print stacktrace
+// if (!isProduction) {
+//   app.use(function(err, req, res, next) {
+//     console.log(err.stack);
+
+//     res.status(err.status || 500);
+
+//     res.json({'errors': {
+//       message: err.message,
+//       error: err
+//     }});
+//   });
+// }
+
+// // production error handler
+// // no stacktraces leaked to user
+// app.use(function(err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.json({'errors': {
+//     message: err.message,
+//     error: {}
+//   }});
+// });
+
+// // finally, let's start our server...
+// var server = app.listen( process.env.PORT || 3000, function(){
+//   console.log('Listening on port: ' + server.address().port);
+// });
+
+const app = require('express')();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+	// this is the route that will be called when the user goes to the root of the site"
+	res.send('Hello World!');
 });
 
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (!isProduction) {
-  app.use(function(err, req, res, next) {
-    console.log(err.stack);
-
-    res.status(err.status || 500);
-
-    res.json({'errors': {
-      message: err.message,
-      error: err
-    }});
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({'errors': {
-    message: err.message,
-    error: {}
-  }});
-});
-
-// finally, let's start our server...
-var server = app.listen( process.env.PORT || 3000, function(){
-  console.log('Listening on port: ' + server.address().port);
+app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`);
 });
